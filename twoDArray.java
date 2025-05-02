eda59978bc95e58ba153a09c6caf4cfeeb4a5e9d
Ashik <ashikece8@gmail.com>
2025-05-02 18:14:16 +0530

    Search in 2D array

---
+++ twoDArray.java
@@ -0,0 +1,30 @@
+// search the target element in the 2d array
+
+import java.util.Arrays;
+
+public class twoDArray {
+
+    static int[] searchElement(int[][] arr, int target) {
+
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                if (arr[row][col] == target)
+                    return new int[] { row, col };
+            }
+        }
+        return new int[] { -1, -1 };
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {
+                { 23, 4, 1 },
+                { 18, 12, 3, 9 },
+                { 78, 99, 34, 56 },
+                { 18, 12 }
+        };
+
+        int target = 34;
+        int[] res = searchElement(arr, target); // format of return value {row, col}
+        System.out.println(Arrays.toString(res));
+    }
+}
