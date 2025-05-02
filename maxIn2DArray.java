219c2f45b5a4004e75493a1043e77ea54f666514
Ashik <ashikece8@gmail.com>
2025-05-02 20:10:23 +0530

    Find the Maximum element in  2D Array

---
+++ maxIn2DArray.java
@@ -0,0 +1,27 @@
+// Find the max element in the 2D Array
+public class maxIn2DArray {
+
+    static int maxElement(int[][] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                if (arr[row][col] > max) {
+                    max = arr[row][col];
+                }
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {
+                { 23, 4, 1 },
+                { 18, 12, 3, 9 },
+                { 78, 1, 34, 56 },
+                { 18, 12 }
+        };
+
+        System.out.println(maxElement(arr));
+
+    }
+}
